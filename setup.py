d903d68728419d9f82c32ed72b9b1ae18d90ab8a
iambrookedrake <iambrookedrake@gmail.com>
2020-07-09 18:02:05 -0600

    Version002

---
+++ setup.py
@@ -0,0 +1,24 @@
+# setup.py file
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lambdata-iambrookedrake",
+    version="0.0.2",
+    author="Brooke Drake",
+    author_email="iambrookedrake@gmail.com",
+    description="Common DS Utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iambrookedrake/lambdata-iambrookedrake",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
