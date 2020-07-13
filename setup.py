da59a5f842de6c972a746cedb0e7792c51a2f666
iambrookedrake <iambrookedrake@gmail.com>
2020-07-12 20:33:52 -0600

    debug

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="lambdata-iambrookedrake",
-    version="0.1.0",
+    name="lambdata_iambrookedrake",
+    version="0.1.1",
     author="Brooke Drake",
     author_email="iambrookedrake@gmail.com",
     description="Common DS Utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/iambrookedrake/lambdata-iambrookedrake",
+    url="https://github.com/iambrookedrake/lambdata_iambrookedrake",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
