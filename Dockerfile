c5fd9ebd71291ece43a1534eeadc7f1055726845
iambrookedrake <iambrookedrake@gmail.com>
2020-07-14 23:20:02 -0600

    addresssplitter

--- Dockerfile
+++ Dockerfile
@@ -13,5 +13,6 @@ RUN apt-get update && \
   apt-get upgrade -y && \
   apt-get install python3-pip curl -y && \
   pip3 install pipenv && \
-  pipenv install pandas 
+  pipenv install pandas && \
+  #pip3 install -i https://test.pypi.org/simple/ lambdata-dspt6==0.0.2
 
--- lambdata_iambrookedrake/AddressSplit.py
+++ lambdata_iambrookedrake/AddressSplit.py
@@ -2,11 +2,14 @@ import usaddress
 # Split address into multiple columns
 from pdb import set_trace as breakpoint
 
+class AddressSplitter():
+
+    def __init__(self, address):
+        self.address = address
 
 
-
-def address_split(address):
-    return usaddress.tag(address)
+    def address_split(address):
+        return usaddress.tag(address)
 
 if __name__ == '__main__':
     address = input("Address Column: ")
